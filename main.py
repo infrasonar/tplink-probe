f892e0d98073f731bef000475a377386560d6b41
Koos85 <koosjoosten@gmail.com>
2025-12-30 12:58:42 +0100

    add checks

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from libprobe.probe import Probe
+from lib.check.cpu import CheckCPU
+from lib.check.memory import CheckMemory
 from lib.check.system import CheckSystem
 from lib.version import __version__ as version
 
 
 if __name__ == '__main__':
     checks = (
+        CheckCPU,
+        CheckMemory,
         CheckSystem,
     )
 
